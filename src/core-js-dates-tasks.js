985b6c594f8db198f2c28390bbf46a47f19cd89f
Dima Berry <dimaberry1@gmail.com>
2024-03-31 23:55:41 +0200

    feat: solve returns the total number of weekend days

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -159,8 +159,15 @@ function isDateInPeriod(date, period) {
  * '1999-01-05T02:20:00.000Z' => '1/5/1999, 2:20:00 AM'
  * '2010-12-15T22:59:00.000Z' => '12/15/2010, 10:59:00 PM'
  */
-function formatDate(/* date */) {
-  throw new Error('Not implemented');
+function formatDate(date) {
+  const year = new Date(date).getFullYear();
+  const month = new Date(date).getMonth() + 1;
+  const day = new Date(date).getUTCDate();
+  const time = new Date(date).toLocaleTimeString('en-US', {
+    timeZone: 'UTC',
+    hour12: true,
+  });
+  return `${month}/${day}/${year}, ${time}`;
 }
 
 /**
