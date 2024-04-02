432ddbf0dea6051e059b9733336c002856962bba
Dima Berry <dimaberry1@gmail.com>
2024-04-02 19:19:33 +0200

    feat: solve generates an employees work schedule within a specified date range, based on a pattern of working

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -283,8 +283,34 @@ function getQuarter(date) {
  * { start: '01-01-2024', end: '15-01-2024' }, 1, 3 => ['01-01-2024', '05-01-2024', '09-01-2024', '13-01-2024']
  * { start: '01-01-2024', end: '10-01-2024' }, 1, 1 => ['01-01-2024', '03-01-2024', '05-01-2024', '07-01-2024', '09-01-2024']
  */
-function getWorkSchedule(/* period, countWorkDays, countOffDays */) {
-  throw new Error('Not implemented');
+function getWorkSchedule(period, countWorkDays, countOffDays) {
+  function periodOfDate(date) {
+    const [day, month, year] = date.split('-').map(Number);
+    return new Date(year, month - 1, day);
+  }
+
+  function dateToSchedule(date) {
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const year = date.getFullYear();
+    return `${day}-${month}-${year}`;
+  }
+
+  const currentDay = periodOfDate(period.start);
+  const end = periodOfDate(period.end);
+  const schedule = [];
+
+  while (currentDay <= end) {
+    for (let i = 0; i < countWorkDays; i += 1) {
+      if (currentDay <= end) {
+        schedule.push(dateToSchedule(currentDay));
+        currentDay.setDate(currentDay.getDate() + 1);
+      }
+    }
+    currentDay.setDate(currentDay.getDate() + countOffDays);
+  }
+
+  return schedule;
 }
 
 /**
